c2f849f3a5a1df4bec135c7c0c825a4ec6b73809
malin@google.com <malin@google.com@6b2f1ad5-4f02-c2ba-490a-2ac32d479fc7>
2010-09-07 23:04:00 +0000

    fixed log property location
    git-svn-id: http://rocksteady.googlecode.com/svn/trunk@94 6b2f1ad5-4f02-c2ba-490a-2ac32d479fc7

--- run_dev.sh
+++ run_dev.sh
@@ -1,2 +1,2 @@
 # Run this in target directory
-mvn -f ../pom.xml compile && java -Xms32m -Xmx128m -cp libs/*:classes/ -Dlog4j.configuration=META-INF/spring/log4j.properties com.admob.rocksteady.Main
+mvn -f ../pom.xml compile && java -Xms32m -Xmx128m -cp libs/*:classes/ -Dlog4j.configuration=META-INF/log4j.properties com.admob.rocksteady.Main
