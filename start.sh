509ad1bc7423917a0bf4b5ab0f31d7ef141f2d93
malin@google.com <malin@google.com@6b2f1ad5-4f02-c2ba-490a-2ac32d479fc7>
2010-10-04 17:50:21 +0000

    switched license to GPL V2
    git-svn-id: http://rocksteady.googlecode.com/svn/trunk@113 6b2f1ad5-4f02-c2ba-490a-2ac32d479fc7

--- start.sh
+++ start.sh
@@ -2,19 +2,5 @@
 
 BASEDIR=`dirname $0`
 
-if [ ! -f $BASEDIR/libs/esper-3.3.0.jar ]; then
-  echo "Downloading Esper..."
-  if [ ! `which wget` ]; then
-    echo "Please install wget in order to download libraries"
-    exit 1
-  fi
-  wget -O $BASEDIR/libs/esper-3.3.0.jar http://repository.codehaus.org/com/espertech/esper/3.3.0/esper-3.3.0.jar
-fi
-
-if [ ! -f $BASEDIR/libs/esper-3.3.0.jar ]; then
-  echo "ERROR: Missing Esper library(3.3.0), please download from http://esper.codehaus.org/esper/download/download.html"
-  exit 1
-fi
-
 # Run this under extract path of rocksteady
 java -Xms32m -Xmx128m -cp libs/*:classes/ -Dlog4j.configuration=META-INF/log4j.properties com.admob.rocksteady.Main
